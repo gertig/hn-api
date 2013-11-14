80476982092eee2057b2f4375e7010b94b5c979b
AndrewGertig <andrew.gertig@gmail.com>
2013-11-13 21:09:34 -0500

    fix unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,12 @@
 # config/unicorn.rb
 
-if Rails.env.production?
+root = "/home/deployer/apps/hnapi/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
 
-	root = "/home/deployer/apps/hnapi/current"
-	working_directory root
-	pid "#{root}/tmp/pids/unicorn.pid"
-	stderr_path "#{root}/log/unicorn.log"
-	stdout_path "#{root}/log/unicorn.log"
-	listen "/tmp/unicorn.hnapi.sock"
-	
-end
-
+listen "/tmp/unicorn.hnapi.sock"
 
 worker_processes 2
 timeout 30
@@ -22,30 +18,3 @@ timeout 30
 before_exec do |_|
   ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
 end
-
-############# ABOVE THIS LINE WAS ADDED FOR VPS ################
-
-# worker_processes 3
-# timeout 30
-# preload_app true
-
-# before_fork do |server, worker|
-
-#   Signal.trap 'TERM' do
-#     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-#     Process.kill 'QUIT', Process.pid
-#   end
-
-#   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-# end
-
-# after_fork do |server, worker|
-
-#   Signal.trap 'TERM' do
-#     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
-#   end
-
-#   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-# end
-
-
