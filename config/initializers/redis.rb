7a6d913135b0433f9236a4c64ad36008644a6f07
AndrewGertig <andrew.gertig@gmail.com>
2013-11-13 22:41:14 -0500

    debugging

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,6 +3,10 @@ require "uri"
 
 REDIS_URL = ENV["REDISTOGO_URL"] 
 
+puts "&&&&&&&&&$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+puts "REDS URL is = "
+puts REDIS_URL
+
 if REDIS_URL != nil
   uri = URI.parse(REDIS_URL)
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
