5d88bd8b2b6bb803fb06a66614b8f818bd5f9316
AndrewGertig <andrew.gertig@gmail.com>
2013-11-16 23:37:49 -0500

    remove dogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 HnApi::Application.routes.draw do
 
-  resources :dogs
-
   api_version(:module => "v1", :header => {:name => "Accept", 
                                :value => "application/vnd.labs.gertig.com; version=1"}, 
                                :parameter => {:name => "version", :value => "v1"}) do
